b12c2238d42b8e0601225e0895ffbaf7a27a46dc
Faysal <74490913+faysalchowdhury99@users.noreply.github.com>
2021-02-25 19:09:37 +0600

    Add Some Features

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,22 @@ const dotIcon = document.querySelector('.dot-icon');
 navbarToggler.addEventListener('click', () => {
     dotIcon.classList.toggle('rotate');
 });
+
+// Back to top Button
+const backToTopBtn = document.getElementById('back-to-top');
+window.onscroll = function () {
+    scrollFunction();
+};
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backToTopBtn.style.opacity = '1';
+        backToTopBtn.style.transform = ' translateY(0)';
+    } else {
+        backToTopBtn.style.opacity = '0';
+        backToTopBtn.style.transform = ' translateY(-300px)';
+    }
+}
+backToTopBtn.addEventListener('click', () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
